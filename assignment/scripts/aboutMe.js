5012730d1aa2c460d645d1ff8f428f06efeba36f
Yailene Alvarez <yailene.alvarez@gmail.com>
2023-12-09 14:59:32 -0500

    required feature 1 complete

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,6 +1,8 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = ' Yailene ';
+console.log( firstName );
+
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
